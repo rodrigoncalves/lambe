2076a6ba460297b2370e75d6fb68e10909f0f13e
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2020-03-19 14:10:41 -0300

    143. Tela de Feeds

---
+++ src/screens/Feed.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react'
+import {StyleSheet, FlatList, View} from 'react-native'
+import Header from '../components/Header'
+import Post from '../components/Post'
+
+export default class Feed extends Component {
+  state = {
+    posts: [
+      {
+        id: Math.random(),
+        nickname: 'Rafael Pereira Filho',
+        email: 'rafaelprrflh@gmail.com',
+        image: require('../../assets/imgs/fence.jpg'),
+        comments: [
+          {
+            nickname: 'John Ray Sheldon',
+            comment: 'Stunning!',
+          },
+          {
+            nickname: 'Ana Julia Arruda',
+            comment: 'Foto linda! Onde foi tirada?',
+          },
+        ],
+      },
+      {
+        id: Math.random(),
+        nickname: 'Francisco Leandro Lima',
+        email: 'fllima@gmail.com',
+        image: require('../../assets/imgs/bw.jpg'),
+        comments: [],
+      },
+    ],
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header />
+        <FlatList
+          data={this.state.posts}
+          keyExtractor={item => `${item.id}`}
+          renderItem={({item}) => <Post key={item.id} {...item} />}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#f5fcff',
+  },
+})
