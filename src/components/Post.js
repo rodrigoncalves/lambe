8154b91dea89e65afaf1d6d3f53dc9f259d7b00e
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2020-03-18 22:50:25 -0300

    140. Componente Autor

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
 import {View, Image, StyleSheet, Dimensions} from 'react-native'
+import Author from './Author'
 
 export default class Post extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Image source={this.props.image} style={styles.image} />
+        <Author email={'fulano@gmail.com'} nickname={'Fulano de Tal'} />
       </View>
     )
   }
