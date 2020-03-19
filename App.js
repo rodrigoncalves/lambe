400103fa4fac8cd21febc0d75be78ac7b1ab0517
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2020-03-18 22:36:35 -0300

    139. Componente Post

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
 import React, {Component} from 'react'
 import Header from './src/components/Header'
+import {View, Text, StyleSheet} from 'react-native'
+import Post from './src/components/Post'
+
+const fence = require('./assets/imgs/fence.jpg')
 
 export default class App extends Component {
   render() {
-    return <Header />
+    return (
+      <View style={styles.container}>
+        <Header />
+        <Post image={fence} />
+      </View>
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
