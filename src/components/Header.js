0747cc63eecc590f62305af97bb4785670b07166
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2020-03-20 17:31:08 -0300

    155. Adicionando Usuário no Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react'
 import {StyleSheet, Text, View, Platform, Image} from 'react-native'
 import icon from '../../assets/imgs/icon.png'
+import {Gravatar} from 'react-native-gravatar'
+import {connect} from 'react-redux'
 
-export default class Header extends Component {
+class Header extends Component {
   render() {
+    const name = this.props.name || 'Anonymous'
+    const gravatar = this.props.email ? (
+      <Gravatar
+        options={{email: this.props.email, secure: true}}
+        style={styles.avatar}
+      />
+    ) : null
     return (
       <View style={styles.container}>
         <View style={styles.rowContainer}>
           <Image source={icon} style={styles.image} />
           <Text style={styles.title}>Lambe Lambe</Text>
+        </View>
+        <View style={styles.userContainer}>
+          <Text style={styles.user}>{name}</Text>
+          {gravatar}
         </View>
       </View>
     )
@@ -21,8 +34,10 @@ const styles = StyleSheet.create({
     padding: 10,
     borderBottomWidth: 1,
     borderColor: '#bbb',
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
-
   rowContainer: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -34,10 +49,28 @@ const styles = StyleSheet.create({
   },
   title: {
     color: 'black',
-    // Android nome do arquivo. iOS nome da fonte.
+    // Android: nome do arquivo. iOS: nome da fonte.
     // Nesse caso os dois são o mesmo.
     fontFamily: 'shelter',
     height: 30,
     fontSize: 28,
   },
+  userContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  user: {
+    fontSize: 10,
+    color: '#888',
+  },
+  avatar: {
+    width: 30,
+    height: 30,
+    marginLeft: 10,
+    borderRadius: 15,
+  },
 })
+
+const mapStateToProps = ({user}) => ({...user})
+
+export default connect(mapStateToProps)(Header)
